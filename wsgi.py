834b217c66087d157a74a4cf503530be66ba3c83
danielcampa <danielcampanella@yahoo.com.br>
2024-08-04 18:25:32 -0300

    Initial Commit

---
+++ wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for duopen project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'duopen.settings')
+
+application = get_wsgi_application()
